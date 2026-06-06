85b415fec7f9b5142ce3bbd3ad7d4cdb594248f9
Kauan Galvani <kauanrafaelgalvani@gmail.com>
2026-06-06 14:14:04 -0300

    adicionado o modulo lista de Compra

--- ListaDeCompra.WebApp/obj/Debug/net10.0/ListaDeCompra.WebApp.AssemblyInfo.cs
+++ ListaDeCompra.WebApp/obj/Debug/net10.0/ListaDeCompra.WebApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ListaDeCompra.WebApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7250fefed4565f31b4ed0fa738af2cae9c8295c0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6b9f45efa5b403f78d4654a7cab562cdcae9ee88")]
 [assembly: System.Reflection.AssemblyProductAttribute("ListaDeCompra.WebApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ListaDeCompra.WebApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
